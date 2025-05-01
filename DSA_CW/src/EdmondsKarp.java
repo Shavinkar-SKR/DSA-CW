fe95535b218f32733f541020b3bd4f6e1892dbe3
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-05-01 22:04:21 +0530

    Updated the algorithm to find max flow

--- DSA_CW/src/EdmondsKarp.java
+++ DSA_CW/src/EdmondsKarp.java
@@ -9,4 +9,70 @@ class EdmondsKarp {
     public EdmondsKarp(FlowNetwork network) {
         this.network = network;
     }
+
+    public int findMaxFlow(int source, int sink) {
+        int maxFlow = 0;
+        int iteration = 0;
+
+        while (true) {
+            iteration++;
+
+            int[] parent = new int[network.getNumNodes()];
+            Edge[] parentEdge = new Edge[network.getNumNodes()];
+            Arrays.fill(parent, -1);
+
+            Queue<Integer> queue = new LinkedList<>();
+            queue.offer(source);
+            parent[source] = -2;
+
+            while (!queue.isEmpty() && parent[sink] == -1) {
+                int current = queue.poll();
+
+                for (Edge edge : network.getEdges(current)) {
+                    int next = edge.getTo();
+
+                    if (parent[next] == -1 && edge.getResidualCapacity() > 0) {
+                        parent[next] = current;
+                        parentEdge[next] = edge;
+                        queue.offer(next);
+                    }
+                }
+            }
+
+            if (parent[sink] == -1) {
+                break;
+            }
+
+            List<Integer> path = new ArrayList<>();
+            for (int node = sink; node != source; node = parent[node]) {
+                path.add(node);
+            }
+            path.add(source);
+            Collections.reverse(path);
+            augmentingPaths.add(path);
+
+            int bottleneck = Integer.MAX_VALUE;
+            for (int node = sink; node != source; node = parent[node]) {
+                Edge edge = parentEdge[node];
+                bottleneck = Math.min(bottleneck, edge.getResidualCapacity());
+            }
+            pathFlows.add(bottleneck);
+
+            for (int node = sink; node != source; node = parent[node]) {
+                Edge edge = parentEdge[node];
+                edge.addFlow(bottleneck);
+            }
+
+            maxFlow += bottleneck;
+
+            System.out.println("Iteration " + iteration + ": Found path with flow " + bottleneck + " (Total: " + maxFlow + ")");
+        }
+
+        System.out.println("\nSummary of augmenting paths:");
+        for (int i = 0; i < augmentingPaths.size(); i++) {
+            System.out.println("Path " + (i+1) + ": " + augmentingPaths.get(i) + " - Flow: " + pathFlows.get(i));
+        }
+
+        return maxFlow;
+    }
 }
